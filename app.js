5d4faef6c4750c894284d88cdd96cc75a5c6bb6c
Randy Duran <randyduranperez@gmail.com>
2025-02-19 22:31:08 +0100

    added functionality to the nav bar

--- app.js
+++ app.js
@@ -17,3 +17,42 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// Hero Slider functionality
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  // Show first slide
+  slides[0].classList.add("active");
+
+  // Change slide every 4 sec
+  setInterval(function () {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+  }, 4000);
+
+  // Navbar functionality
+  let lastScrollTop = 0;
+  const navbar = document.querySelector(".navbar");
+  const navbarHeight = navbar.offsetHeight;
+
+  window.addEventListener("scroll", function () {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > 50) {
+      navbar.classList.add("nav-scrolled");
+    } else {
+      navbar.classList.remove("nav-scrolled");
+    }
+
+    if (scrollTop > lastScrollTop && scrollTop > navbarHeight) {
+      navbar.classList.add("nav-hidden");
+    } else {
+      navbar.classList.remove("nav-hidden");
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  });
+});
